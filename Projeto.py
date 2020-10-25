67739741e163503c50c338d450197b1d07c51ab7
oliveirats <67207802+oliveirats@users.noreply.github.com>
2020-10-25 19:44:17 -0300

    Versão 1

---
+++ Projeto.py
@@ -0,0 +1,10 @@
+import streamlit as str
+str.title("Buscador de Vagas PcD")
+str.subheader("Qual vaga você procura?")
+str.text_input("Digite aqui o cargo ideal para você:") 
+str.button("BUSCAR")
+
+
+
+
+str.text("Grupo Zênite")
